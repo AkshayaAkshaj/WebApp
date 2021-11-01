7c8c64970e609fe3417399b7d9744ba508ba05bc
AkshayaAkshaj <87810890+AkshayaAkshaj@users.noreply.github.com>
2021-11-01 13:48:22 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,7 +1,14 @@
 
 
+  
+
   function add_user(){
-    username=document.getElementById("username").value ;
-    localStorage.setItem("username",username);
-    window.location= "chat_room.html";
+    user_name=document.getElementById("input_user_name").value ;
+    
+    window.location="chat_room.html";
+    localStorage.setItem("user_name",user_name);
+
+    firebase.database().ref("/").child(user_name).update({
+      purpose:"Adding User Name"
+     });
   }
